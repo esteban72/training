6b27269fad7fa7a1310e05f840d3af3452c6ec2c
esteban72 <esteban.ing72@gmail.com>
2017-04-09 21:54:01 -0500

    prueba subiendo un archivo con extension
    javascript

---
+++ challenges/coderbyte/easy/esteban72.js
@@ -0,0 +1,14 @@
+function LongestWord(sen) { 
+
+  
+  var arr = sen.match(/[a-z0-9]+/gi);
+  
+  var sorted = arr.sort(function(a, b) {
+    return b.length - a.length;
+  });
+
+  return sorted[0];
+         
+}
+    
+LongestWord(readline());
